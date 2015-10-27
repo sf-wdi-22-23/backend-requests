b67b2073127df0e8ed8ac13223b84accf9df8d40
Brianna Veenstra <bgveenstra@users.noreply.github.com>
2015-10-27 13:49:08 -0700

    comments

--- server.js
+++ server.js
@@ -1,33 +1,35 @@
+//server.js
 
+// load in the express module
 var express = require('express');
+// load in the request module
 var request = require('request');
 // request.debug = true;
 
-// don't need to save to a var
+// don't need to save this one to a var
 // just pull in the dotenv module's code and run its load method
 // you can save to a variable to check what is being requried if curious
 require('dotenv').load();  
 
-
+// create app object
 var app = express();
 
-GIPHY_API_KEY = 'dc6zaTOxFJmzC';
+// don't keep actual secrets on github! move to .env file and GITIGNORE THE .ENV FILE!!!!!
+GIPHY_API_KEY = 'dc6zaTOxFJmzC';  
 giphySearchBaseURL = 'http://api.giphy.com/v1/gifs/search?';
 
 
-var queryStringObj = {
-	api_key: process.env.GIPHY_API_KEY,
-	q: 'cats'
-}
-
-var requestOptions = {
-	uri: giphySearchBaseURL,
-	//method: 'GET', // the default, so not required
-	qs: queryStringObj
-}
-
-
 app.get('/', function(req, res){
+	var queryStringObj = {
+		api_key: process.env.GIPHY_API_KEY || GIPHY_API_KEY, // still works if using the public key (above) or a private api key
+		q: 'cats'
+	}
+
+	var requestOptions = {
+		uri: giphySearchBaseURL,
+		//method: 'GET', // the default, so not required
+		qs: queryStringObj
+	}
 	request(requestOptions, function(error, response, body){
 		console.log(response);
 		var JSON_body = JSON.parse(response.body);
